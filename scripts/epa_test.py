6cc333a61b9245d2464ca6432cf77d5d35878b88
buddha314 <buddha_314@protonmail.com>
2022-07-18 16:35:13 -0700

    fully loads a postgres database of choice

--- scripts/epa_test.py
+++ scripts/epa_test.py
@@ -3,6 +3,8 @@ import wget
 import zipfile
 from dotenv import load_dotenv
 import psycopg2
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 
@@ -16,10 +18,31 @@ def download_and_unzip(wget_filelist, dir):
                 z.extractall(dir)
 
 def process_downloaded_files(dir):
+    #conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)
+    conn_string = 'postgresql://{u}:{p}@127.0.0.1/{db}'.format(u=DB_USER, p=DB_PASSWORD, db=DB_NAME)
+    print(conn_string)
+    db = create_engine(conn_string)
+    conn = db.connect()
     for file in glob.glob(dir + "/*.csv"):
-        print(DB_NAME)
-        conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)
-        print(file)
+        #print(DB_NAME)
+        # Turn the file name into a table name
+        tname = file.replace(EPA_TMP_DIR + "/", '').replace('.csv', '').replace('-', '_').lower().strip()
+        print(file + "\t" + tname)
+        #df = pd.read_csv(file)
+        df = pd.read_csv(file, dtype={'REGISTRY_ID': 'str'
+            , 'NAICS_CODE': 'str'
+            , 'SRS_ID': 'str'
+            , 'FAC_STATE': 'str'
+            , 'FAC_ZIP': 'str'
+            , 'POLLUTANT_CODES':'str'
+            , 'PGM_SYS_ID': 'str'
+            , 'NEI_TYPE': 'str'
+            , 'NEI_HAP_VOC_FLAG': 'str'
+            })
+        try:
+            df.to_sql(name=tname, con=conn, schema=EPA_SCHEMA, if_exists='fail')
+        except:
+            print("table {t} already exists".format(t=tname))
 
 
 if __name__=="__main__":
@@ -32,6 +55,7 @@ if __name__=="__main__":
     DB_NAME = os.getenv("DB_NAME")
     DB_USER= os.getenv("DB_USER")
     DB_PASSWORD= os.getenv("DB_PASSWORD")
+    EPA_SCHEMA = os.getenv("EPA_SCHEMA")
     try:
         tdir = os.mkdir(EPA_TMP_DIR)
     except:
