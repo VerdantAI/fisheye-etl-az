e099bf602f782efda4f2d95d9187ecb0a6006835
buddha314 <buddha_314@protonmail.com>
2022-07-17 21:17:37 -0700

    connected to local db not loaded yet

--- scripts/epa_test.py
+++ scripts/epa_test.py
@@ -1,9 +1,13 @@
 import os, glob
 import wget
 import zipfile
+from dotenv import load_dotenv
+import psycopg2
+
 
 
 def download_and_unzip(wget_filelist, dir):
+    print("This grabs a LOT of data, so be patient.")
     with open(wget_filelist, 'r') as f:
         for line in f.readlines():
             print(line)
@@ -13,13 +17,21 @@ def download_and_unzip(wget_filelist, dir):
 
 def process_downloaded_files(dir):
     for file in glob.glob(dir + "/*.csv"):
+        print(DB_NAME)
+        conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD)
         print(file)
 
 
 if __name__=="__main__":
-    print("HELLO FISHEYE EPA!  Run from the Fisheye ETL root directory.  This grabs a LOT of data, so be patient.")
+    print("HELLO FISHEYE EPA!  Run from the Fisheye ETL root directory.")
+    load_dotenv()
+    
+    # This hold environment data, including the local database information"
     epa_zipfile_list = "./epa/file_list.csv"
-    EPA_TMP_DIR = "epa_tmp"
+    EPA_TMP_DIR = os.getenv('EPA_TMP_DIR')
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER= os.getenv("DB_USER")
+    DB_PASSWORD= os.getenv("DB_PASSWORD")
     try:
         tdir = os.mkdir(EPA_TMP_DIR)
     except:
